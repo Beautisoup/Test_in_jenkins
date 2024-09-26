f5f10c603a8b88a44292ca07523f51f13a8036b9
liyuanrui <1339549930@qq.com>
2024-09-26 15:14:01 +0800

    test_in_jenkins

---
+++ MY_TestPlat/TestPlat_flask/tests/test_user_controller.py
@@ -0,0 +1,58 @@
+from urllib.parse import urljoin
+
+import pytest
+import requests
+
+base_url = 'http://127.0.0.1:5055/testcase'
+user_list = [
+    {
+        'username': 'AA',
+        'password': '123456'
+    },
+    {
+        'username': 'BB',
+        'password': '123456'
+    },
+    {
+        'username': 'CC',
+        'password': '123456'
+    }
+]
+class TestUserController:
+    @pytest.mark.parametrize('user', user_list)
+    def test_login(self,user):
+        #拼接url
+        url = urljoin(base_url, '/user/login')
+        #取出用户名,密码
+        username= user.get('username')
+        password= user.get('password')
+        data = {
+            'username': username,
+            'password': password
+        }
+        #发送请求
+        response = requests.post(url, json=data)
+        print(response.json())
+        the_json = response.json()
+        assert the_json['code'] == 0,the_json['msg']=='login success'
+
+    # @pytest.mark.parametrize('user', user_list)
+    # def test_register(self,user):
+    #     url = urljoin(base_url, '/user/register')
+    #     username= user.get('username')
+    #     password= user.get('password')
+    #     data = {
+    #         'username': username,
+    #         'password': password
+    #     }
+    #     response = requests.post(url, json=data)
+    #     print(response.json())
+
+    '''
+     pytest -sv test_user_controller.py --alluredir=allure_data
+     
+     allure serve allure_data
+
+     allure generate allure_data -o allure_report --clean
+     allure open allure_report
+    '''
