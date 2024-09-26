6b7e229175ae796428a9cd0cd7c8b87459096659
liyuanrui <1339549930@qq.com>
2024-09-26 10:58:25 +0800

    test_in_jenkins

--- MY_TestPlat/TestPlat_flask/utils/test_in_jenkins.py
+++ MY_TestPlat/TestPlat_flask/utils/test_in_jenkins.py
@@ -2,6 +2,6 @@ import pytest
 
 
 def test_addition():
-    assert 1 + 1 == 2
+    assert 1 + 1 == 2, "Addition test failed"
 if __name__ == '__main__':
     pytest.main(['--alluredir=allure-results'])
